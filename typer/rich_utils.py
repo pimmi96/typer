c2ea9d8d24b8480dd2a99c2684d1be96d90abfb7
Giovanni Bocchi <53173171+pimmi96@users.noreply.github.com>
2022-10-22 12:28:10 +0200

    Update rich_utils.py

--- typer/rich_utils.py
+++ typer/rich_utils.py
@@ -192,8 +192,7 @@ def _get_help_text(
     first_line = help_text.split("\n\n")[0]
     # Remove single linebreaks
     if markup_mode != MARKUP_MODE_MARKDOWN and not first_line.startswith("\b"):
-        pass
-        #first_line = first_line.replace("\n", " ")
+        first_line = first_line.replace("\n", " ")
     yield _make_rich_rext(
         text=first_line,#.strip(),
         style=STYLE_HELPTEXT_FIRST_LINE,
@@ -205,12 +204,12 @@ def _get_help_text(
     if remaining_paragraphs:
         if markup_mode != MARKUP_MODE_RICH:
             # Remove single linebreaks
-            #remaining_paragraphs = [
-            #    x.replace("\n", " ").strip()
-            #    if not x.startswith("\b")
-            #    else "{}\n".format(x.strip("\b\n"))
-            #    for x in remaining_paragraphs
-            #]
+            remaining_paragraphs = [
+                x.replace("\n", " ").strip()
+                if not x.startswith("\b")
+                else "{}\n".format(x.strip("\b\n"))
+                for x in remaining_paragraphs
+            ]
             # Join back together
             remaining_lines = "\n".join(remaining_paragraphs)
         else:
